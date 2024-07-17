8042653355b8ecea6938db58aaa23df043de475a
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-07-17 14:13:20 -0300

    Create Istruction_memory.v

---
+++ Istruction_memory.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module instruction_memory (
+    input [31:0] address,
+    output [31:0] instruction);
+    
+    reg [7:0] memory[1023:0];
+    
+    assign instruction = {memory[address], memory[address + 1], memory[address + 2], memory[address + 3]};
+	         
+endmodule
