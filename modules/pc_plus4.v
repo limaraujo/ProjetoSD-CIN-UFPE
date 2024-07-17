65feaa59e23d98d5258cc4c219fb0d64d2b51c3d
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-07-17 14:21:44 -0300

    Create pc_plus4.v

---
+++ modules/pc_plus4.v
@@ -0,0 +1,16 @@
+module PC (
+    input wire clk,               // Clock signal
+    input wire reset,             // Reset signal
+    output reg [31:0] pc          // Current PC value
+);
+
+    // Initialize PC to zero on reset
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            pc <= 32'b0;           // Reset PC to 0
+        end else begin
+            pc <= pc + 4;          // Increment PC by 4 on each clock cycle
+        end
+    end
+
+endmodule
